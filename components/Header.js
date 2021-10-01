0b770c956d215e3505e346dd99f9862cbd5fb74f
Thalitafs7 <thalitafeitosadsilva@gmail.com>
2021-09-30 22:28:30 -0300

    SIM

---
+++ components/Header.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import ImageList from './ImageList';
+
+const uriListAll = "https://dog.ceo/api/breeds/list/all";
+
+export default function Header(){
+    const [selectedValue, setSelectedValue] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [list, setList] = useState([]);
+    
+    const getAllBreeds = async () => {
+        try {
+            if(isLoading){
+                const response = await fetch(uriListAll);
+                
+                if (response.status != 200) {
+                    console.log(`Erro: ${response.status}`);
+                    return;
+                }
+                
+                const json = await response.json();
+                setList(json);
+            }
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => { 
+        getAllBreeds(); []}
+    );
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>DOG API</Text>
+            <View style={styles.select}>
+                <Picker
+                    selectedValue={selectedValue}
+                    onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+                    <Picker.Item label="Selecione uma raça..." value={null} />
+                    {!isLoading && 
+                        Object.keys(list.message).map((item) => { 
+                            return <Picker.Item label={item} value={item} /> 
+                        })
+                    }
+                </Picker>
+            </View>
+
+            <View style={{ flex: 1, margin: 5 }}>
+                {selectedValue !== null &&
+                    <ImageList breed={selectedValue} />
+                }
+            </View>
+            <StatusBar style="default" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    title: {
+        height: 40,
+        fontSize: 20,
+        color: 'white',
+        backgroundColor: '#1E90FF',
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    select: {
+        borderBottomColor: 'black',
+        borderRadius: 10,
+        borderWidth: 1,
+        padding: 5,
+        margin: 5,
+    }
+});
---
+++ components/ImageList.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Image, ActivityIndicator } from "react-native";
+import Swiper from 'react-native-swiper'
+
+export default function ImageList({ breed }) {
+    const [listURL, setListURL] = useState({});
+    const [isLoading, setLoading] = useState(true);
+    const [nameBreed, setNameBreed] = useState(breed);
+
+    const getByBreed = async () => {
+        try {
+            if(nameBreed !== null){
+                setLoading(true);
+                const response = await fetch(`https://dog.ceo/api/breed/${nameBreed}/images/random/5`);
+    
+                if (response.status != 200) {
+                    console.log(`Erro: ${response.status}`);
+                    return;
+                }
+                const json = await response.json();
+                setListURL(json.message);
+            }
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => { getByBreed(); }, [])
+
+    return (
+        <View style={styles.container}>
+            {listURL !== null &&
+                <Swiper showsButtons loop={false}>
+                    <View testID="0" style={styles.swiperViews}>
+                        {isLoading && <ActivityIndicator size="large" color="blue" />}
+                        {!isLoading &&
+                            <Image source={{ uri: listURL[0] }} style={styles.img} />
+                        }
+                    </View>
+                    <View testID="1" style={styles.swiperViews}>
+                        {isLoading && <ActivityIndicator size="large" color="blue" />}
+                        {!isLoading &&
+                            <Image source={{ uri: listURL[1] }} style={styles.img} />
+                        }
+                    </View>
+                    <View testID="2" style={styles.swiperViews}>
+                        {isLoading && <ActivityIndicator size="large" color="blue" />}
+                        {!isLoading &&
+                            <Image source={{ uri: listURL[2] }} style={styles.img} />
+                        }
+                    </View>
+                    <View testID="3" style={styles.swiperViews}>
+                        {isLoading && <ActivityIndicator size="large" color="blue" />}
+                        {!isLoading &&
+                            <Image source={{ uri: listURL[3] }} style={styles.img} />
+                        }
+                    </View>
+                    <View testID="4" style={styles.swiperViews}>
+                        {isLoading && <ActivityIndicator size="large" color="blue" />}
+                        {!isLoading &&
+                            <Image source={{ uri: listURL[4] }} style={styles.img} />
+                        }
+                    </View>
+                </Swiper>
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        borderRadius: 10,
+        borderWidth: 1,
+        padding: 5,
+        margin: 5
+    },
+    swiperViews: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 2
+    },
+    img: {
+        width: 320,
+        height: 320,
+    }
+});
